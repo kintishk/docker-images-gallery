2b124c4e1186d24035e01870be39e3977ae6e27d
Ken Howard <kendhoward@gmail.com>
2022-02-23 19:21:53 -0500

    docker-compose for front end and api

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -12,7 +12,8 @@ const Search = ({ word, setWord, handleSubmit }) => {
                 <Form.Control
                   type="text"
                   value={word}
-                  onChange={(e) => setWord(e.target.value)}
+                  //onChange={e => setWord(e.target.value)}
+                  onChange={(e, _) => setWord(e.target.value)}
                   placeholder="Search for new image..."
                 />
               </Col>
